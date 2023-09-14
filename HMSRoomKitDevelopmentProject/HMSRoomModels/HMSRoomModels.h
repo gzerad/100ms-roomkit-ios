fae70c23fe70f3dc81697dea08eb6c44d2d9fd59
gzerad <dfedoseyev@gmail.com>
2023-09-14 14:19:01 +0300

    Project setup for importing custom sdk build

---
+++ HMSRoomKitDevelopmentProject/HMSRoomModels/HMSRoomModels.h
@@ -0,0 +1,18 @@
+//
+//  HMSRoomModels.h
+//  HMSRoomModels
+//
+//  Created by Dmitry Fedoseyev on 14.09.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HMSRoomModels.
+FOUNDATION_EXPORT double HMSRoomModelsVersionNumber;
+
+//! Project version string for HMSRoomModels.
+FOUNDATION_EXPORT const unsigned char HMSRoomModelsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HMSRoomModels/PublicHeader.h>
+
+
